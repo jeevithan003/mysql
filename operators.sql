3339fa00654269fe8486caef7406ffce67787631
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-05-11 20:46:39 +0530

    Update operators.sql

--- operators.sql
+++ operators.sql
@@ -10,3 +10,10 @@ AND PostalCode > '12000';
 SELECT * FROM Customers
 WHERE Country = 'Spain'
 AND (CustomerName LIKE 'G%' OR CustomerName LIKE 'R%');
+
+
+SELECT * FROM Customers
+where country <> "argentina";
+
+SELECT * FROM Customers
+WHERE CustomerID NOT BETWEEN 30 AND 90;
